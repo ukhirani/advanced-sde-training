36b3747c83279a505686174a0e009acd671b03c9
Umang Hirani <umanghirani.exe@gmail.com>
2025-09-09 14:51:28 +0530

    removed std namespace

--- prime-rank-number.cpp
+++ prime-rank-number.cpp
@@ -1,3 +1,8 @@
+//question description
+//if there is a list of any prime numbers starting from 2,3,5,7 and so on. the index of the prime number on the list is called it's 'rank'
+// you need to find the count of numbers such that the rank and it's reverse say r1 and r2, the number at these ranks r1 and r2, you can make one out of other by reversing them
+// let's say w1 and w2 are the numbers at r1 and r2 where r1 <- reverse -> r2  then w1 <- revers -> w2
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -25,7 +30,7 @@ int main() {
   for (int i = 2; i < n; i++) {
     if (flags[i]) {
       v.push_back(i);
-      std::cout<<i<<endl;
+      cout<<i<<endl;
       for (int j = 2; (j * i) < n; j++) {
         flags[j * i] = false;
       }
