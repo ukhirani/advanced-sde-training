b48e5b5bf531a5af176bb71a4491afd0e05b8423
Umang Hirani <umanghirani.exe@gmail.com>
2025-09-12 11:01:44 +0530

    did array of structs hw

---
+++ Pointers/array-of-structs.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Person {
+
+  string name;
+  int height;
+  int weight;
+
+  void hello() { cout << "hello " << name; }
+  void setter(string name1, int height1, int weight1) {
+    name = name1;
+    height = height1;
+    weight = weight1;
+  };
+  void name_printer() {
+    cout << name << "\t" << height << "\t" << weight << endl;
+  }
+};
+
+int main() {
+  Person umang, harsh, jay, mahida;
+  Person arr_of_structs[3];
+  Person *arr_of_pointers[3];
+
+  umang.setter("umang", 182, 60);
+  arr_of_structs[0] = umang;
+  arr_of_pointers[0] = &umang;
+
+  harsh.setter("harsh", 181, 75);
+  arr_of_structs[1] = harsh;
+  arr_of_pointers[1] = &harsh;
+
+  jay.setter("jay", 183, 63);
+  arr_of_structs[2] = jay;
+  arr_of_pointers[2] = &jay;
+
+  cout << "Printing through array of structs" << endl;
+  for (auto person : arr_of_structs) {
+    person.name_printer();
+  }
+
+  cout<<endl;
+
+  cout << "Printing through array of pointers" << endl;
+  for (auto person : arr_of_pointers) {
+    person->name_printer();
+  }
+}
