7ada7abcb0f28c582a1e4f47b6bfd1290a53a4a8
Umang Hirani <umanghirani.exe@gmail.com>
2025-10-07 11:46:05 +0530

    minor formattingg changes

--- primes-in-the-range-space-optimized.cpp
+++ primes-in-the-range-space-optimized.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
-#include <climits>
-#include <cmath>
+#include <vector>
+
 using namespace std;
 
-vector<int> sieve(int n) {
-  vector<int> prime(n + 2, true);
+void sieve(vector<int> &prime) {
+  // vector<int> prime(n + 2, 1);
+  int n = prime.size();
   prime[0] = prime[1] = 0;
   for (int i = 2; i < sqrt(n); i++) {
     if (prime[i]==1) {
@@ -13,12 +14,12 @@ vector<int> sieve(int n) {
       }
     }
   }
-  return prime;
+  // return primqe;
 }
 
 int main() {
   
-  constexpr int size = 1e6 + 2;  
+  constexpr int size = 1e6 + 2; 
   vector<int> prefix(size, 0);
   
   int q,max_val;
@@ -35,29 +36,26 @@ int main() {
 
   //start time here
   auto start = std::chrono::high_resolution_clock::now();
-  vector<int> sieve_output = sieve(size);
 
   int sum = 0;
 
   for (int i = 0; i<=max_val; i++) {
-
     prefix[i]+=sum;
     sum = prefix[i];
-  
-
   }
   int count = 0;
+
+  sieve(prefix);
 
   cout<<endl<<"Here is the answer"<<endl;
     for (int i = 1; i<=max_val; i++) {
 
-      if(prefix[i]&&sieve_output[i]){
+      if(prefix[i]){
         cout<<i<<endl;
         count++;
       }
   
   }
-  cout<<count<<endl;
   auto end = std::chrono::high_resolution_clock::now();
   auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
   std::cout << "Time taken: " << elapsed.count() << " ms" << std::endl;
