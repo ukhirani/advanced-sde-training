f6ab7b834b47f2c21d2d59d204f93644803e5594
Umang Hirani <umanghirani.exe@gmail.com>
2025-10-25 19:18:03 +0530

    wrote binary search but in neovim

---
+++ binary-search.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  int target = 56;
+  vector<int> arr = {1, 2, 5, 6, 7, 8, 56};
+  bool found = false;
+  int left = 0, right = arr.size() - 1;
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
+    if (arr[mid] == target) {
+      cout << "element found at " << mid << endl;
+      found = true;
+      break;
+    } else if (target > arr[mid]) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  if (!found) {
+    cout << "element not found";
+  }
+
+  return 0;
+}
